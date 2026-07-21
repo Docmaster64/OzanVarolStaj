4286d60c1cc9c7e843d30f88625f782d346012db
Efe Ahmet Balıkçı <docmaster64@gmail.com>
2026-07-21 11:06:53 +0300

    hero kısmı nın animasyonu kaldı

--- public/build/root-TKD5JC7L.js
+++ public/build/root-TKD5JC7L.js
@@ -25,7 +25,7 @@ import {
 var fonts_default = "/build/_assets/fonts-ECI55RS3.css";
 
 // app/styles/global.css
-var global_default = "/build/_assets/global-ZD6RSSVB.css";
+var global_default = "/build/_assets/global-AXRXPLHH.css";
 
 // app/components/Navigation.tsx
 var import_react = __toESM(require_react(), 1);
@@ -702,4 +702,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-IYEKCSQD.js.map
+//# sourceMappingURL=/build/root-TKD5JC7L.js.map

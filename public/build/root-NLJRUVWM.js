71065a0694bb303953a40e524516c206e91754b5
Efe Ahmet Balıkçı <docmaster64@gmail.com>
2026-07-27 14:15:30 +0300

    audio telefon kısmı bitti

--- public/build/root-NLJRUVWM.js
+++ public/build/root-NLJRUVWM.js
@@ -25,7 +25,7 @@ import {
 var fonts_default = "/build/_assets/fonts-ECI55RS3.css";
 
 // app/styles/global.css
-var global_default = "/build/_assets/global-NSG6N4SE.css";
+var global_default = "/build/_assets/global-XPCZUT7N.css";
 
 // app/components/Navigation.tsx
 var import_react = __toESM(require_react(), 1);
@@ -718,4 +718,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-HII6PXVR.js.map
+//# sourceMappingURL=/build/root-NLJRUVWM.js.map
